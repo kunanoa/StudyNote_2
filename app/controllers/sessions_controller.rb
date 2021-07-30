1566e16feab61d3c2f40341e27939297b56be332
kunanoa <81843512+kunanoa@users.noreply.github.com>
2021-07-30 08:39:26 +0000

    13st commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root_url, info: 'ログアウトしました'
+    redirect_to login_path, info: 'ログアウトしました'
   end
 
   private
